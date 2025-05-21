127d3fb7b3501550be46dec96875834ddbd553a0
manishbisht2000 <manishbisht2000@gmail.com>
2025-05-21 20:52:03 +0530

    initial commit

---
+++ backend/typeDefs/user.typeDef.js
@@ -0,0 +1,24 @@
+const userTypeDef = `#graphql
+    type User {
+        _id: ID!
+        username: String!
+        name: String!
+        password: String!
+        profilePicture: String
+        gender: String!
+    }
+
+    type Query {
+        users: [User!]
+        authUser: User
+        user(userId:ID): User
+    }
+
+    type Mutation {
+        signUp(input: SignUpInput!): User
+        login(input: LoginInput!): User
+        logout: LogoutResponse
+    }
+
+    
+`
